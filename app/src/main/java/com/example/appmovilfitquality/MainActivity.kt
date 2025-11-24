69836fc3a104995760fa95e886e018cfb7ccf30f
CesarHenriquez <ce.henriquezn@duocuc.cl>
2025-11-24 18:56:20 -0300

    Pruebas unitarias y splash screen

--- app/src/main/java/com/example/appmovilfitquality/MainActivity.kt
+++ app/src/main/java/com/example/appmovilfitquality/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.appmovilfitquality
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.appmovilfitquality.navigation.NavGraph
 import com.example.appmovilfitquality.ui.theme.AppMovilFitQualityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
 
